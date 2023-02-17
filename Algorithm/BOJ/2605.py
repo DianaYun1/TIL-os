001fa74bb770480d998ba0de2cc9086b1e635813
Belluable <yuojin1004@naver.com>
2023-02-17 17:21:29 +0900

    0217

--- Algorithm/BOJ/2605.py
+++ Algorithm/BOJ/2605.py
@@ -1,2 +1,8 @@
 n = int(input())
+stu = []
 lst = list(map(int, input().split()))
+
+for i in range(n):
+    stu.insert(i-lst[i], i+1)
+
+print(*stu)
---
+++ Algorithm/BOJ/2628.py
@@ -0,0 +1,9 @@
+w, h = map(int, input().split())
+n = int(input())
+for _ in range(n):
+    di, jum = map(int, input().split())
+    if di == 0:
+        pass
+    elif di == 1:
+        pass
+
